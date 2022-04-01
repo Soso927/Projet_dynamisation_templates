b6724177ee91613b43e36704c0756f2f341946ba
Soso927 <sofianeagoumellah1@gmail.com>
2022-04-01 11:28:46 +0200

    initial commit

---
+++ data/data.php
@@ -0,0 +1,40 @@
+<?php
+
+$stades = array(
+    array(
+        'id'=> 1,
+        'nom stade' => 'Allianz arena',
+        'capacité'=> 75024,
+        'lieux'=> 'Munich',
+        'description' =>'Contrairement à Anfield, pour le plus beau stade de foot d’Allemagne, c’est l’architecture atypique qui place l’Allianz Arena en haut du classement. En termes de ferveur, le stade du Borussia Dortmund est devant mais le stade du Bayern Munich en forme de pneu est plus qu’impressionnant. Surtout si celui-ci est illuminé la nuit.',
+        'pays'=> 'Allemagne',
+        'img'=> '.img/Allianz arena.jpg',
+    ),
+    array(
+        'id'=> 2,
+        'nom du stade' => 'Camp Nou',
+        'capacité'=> 99354,
+        'lieux'=> 'Barcelone',
+        'description' =>'Sans doute à la première place des plus beaux stades d’Europe voire du monde, on retrouve la stade du FC Barcelone, le Camp Nou. Avec une capacité de 99 354 places, le stade catalan est l’un des plus grands d’Europe. En plus de l’ambiance, la hauteur des tribunes donne un aspect particulier et hostile pour les équipes visiteuses.',
+        'pays'=> 'Espagne',
+        'img'=> '.img/Camp nou.jpg',
+    ),
+    array(
+        'id'=> 3,
+        'nom du stade' => 'Stade vélodrome',
+        'capacité'=> 67394,
+        'lieux'=> 'Marseille',
+        'description' =>'En France, c’est le stade Vélodrome de l’Olympique de Marseille qui surclasse tous les autres et dans tous les domaines. Son architecture le rend unique et la ferveur du public marseillais en fait l’un des stades les plus bruyants d’Europe.',
+        'pays'=> 'France',
+        'img'=> '.img/Camp Nou.jpg',
+    ),
+    array(
+        'id'=> 4,
+        'nom du stade' => 'Yohann Cruyf Arena',
+        'capacité'=> 68000,
+        'lieux'=> 'Amsterdam',
+        'description' =>'Rebaptisé ainsi après la mort de Johan Cruyff, véritable légende aux Pays-Bas, le stade d’Amsterdam est l’un des stades les plus modernes de Hollande. Il est en tout cas celui avec la plus grande capacité avec 68 000 places. Et autant vous dire que les soirs de grands matchs, la Johan Cruyff Arena propose l’une des meilleures ambiances d’Europe.',
+        'pays'=> 'Pays-Bas',
+        'img'=> '.img/Yohan Cruyf Arena.jpg',
+    ),
+    ?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require 'partials/header.php';
+?> 
+<?php 
+foreach ($stades as $value): 
+
+
+?>    
+        <div class="card col-3" >
+            <img src=".img/Allianz arena.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Allianz arena </h5>
+                <p class="card-text">Contrairement à Anfield, pour le plus beau stade de foot d’Allemagne, c’est l’architecture atypique qui place l’Allianz Arena en haut du classement. En termes de ferveur, le stade du Borussia Dortmund est devant mais le stade du Bayern Munich en forme de pneu est plus qu’impressionnant. Surtout si celui-ci est illuminé la nuit.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Allemagne.</a></li>
+                <li class="list-group-item">Munich</li>
+                <li class="list-group-item">75025.</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">en savoir plus</a>
+            </div>
+        </div>
+    </div>
+    </main>
+ <?php 
+ require 'partials/footer.php'
+ ?>
